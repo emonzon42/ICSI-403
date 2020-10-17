e6a69019009748a597482e91189e5aa251e2c6e8
emonzon42 <eli.develops@gmail.com>
2020-10-17 01:10:53 -0400

    added mst and driver

---
+++ Homework/HW3/src/DriverMST.java
@@ -0,0 +1,18 @@
+/**
+ * Eli Monzon
+ * 10.16.20
+ * CSI 403
+ * DriverMST
+ */
+public class DriverMST {
+
+    public static void main(String[] args) {
+
+        MST.kruskal();
+
+        if (args.length > 1){
+            //todo:  step 7:
+        //todo: when args[1] = '-e' print list of edges and costs
+        }
+    }
+}
---
+++ Homework/HW3/src/MST.java
@@ -0,0 +1,31 @@
+/**
+ * Eli Monzon
+ * 10.16.20
+ * CSI 403
+ * MST
+ */
+import java.util.*;
+import java.io.*;
+public final class MST {
+
+    public static int[][] gothru(File file){
+        //Todo: read input file and output something useable for the algorithms
+    
+        return new int[2][];
+    }
+
+    public static void kruskal(int[] nodes, int[] edges){
+
+        long tick = System.currentTimeMillis();
+        Arrays.sort(edges);
+        HashSet<Integer> T = new HashSet<>();
+
+        while (true) {
+           //!stuff 
+        }
+        long tock = System.currentTimeMillis();
+
+        System.out.println("Execution Time: (" + (tock-tick) + "ms)");
+    }
+    
+}
