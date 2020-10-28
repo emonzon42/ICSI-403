7b3b75b8674e25f5d1af36f37141f73265cbbeeb
emonzon42 <eli.develops@gmail.com>
2020-10-28 14:20:21 -0400

    added compatibility  with command line args

--- Homework/HW3/src/DriverMST.java
+++ Homework/HW3/src/DriverMST.java
@@ -8,19 +8,21 @@
 import java.io.*;
 import java.util.Collection;
 public class DriverMST {
+    // cd Homework/HW3 
+    //* javac -d "../bin" "DriverMST.java" 
+    //* java DriverMST ../infile.txt -e
 
-    //* javac -d "Homework/HW3/bin" "Homework/HW3/src/DriverMST.java" Homework/HW3/infile.txt
-    private static final String TEST_FILE = "Homework/HW3/infile.txt";
+    private static final String TEST_FILE = "../infile2.txt";
     public static void main(String[] args) {
         
-       /* if (args.length < 1){
-            System.out.println("Please start the program like this: \"MST.py InputFilePathName options\"");
+        if (args.length < 1){
+            System.out.println("Please start the program like this: \"DriverMST InputFilePathName [options]\"");
             return;
-        }*/
+        }
         
         Graph g = null;
         try {
-            File file = new File(TEST_FILE);//= new File(args[0]) //!NEEDS TO WORK W COMMAND LINE BUT COMMAND LINE WAS GMT
+            File file = new File(args[0]);
             g = MST.gothru(file);
             if (g == null)
                 return;
@@ -30,14 +32,15 @@ public class DriverMST {
         }
         System.out.println("Now executing Kruskal's algorithm...");
         Collection<Edge> T = MST.kruskal(g);
-        //if (args.length > 1 && args[1] == "-e")
+        if (args.length > 1 && args[1].equals("-e"))
             System.out.println("Edges: " + T.toString());
 
         System.out.println();
 
         System.out.println("Now executing Prim's algorithm...");
         T = MST.prim(g);
-        //if (args.length > 1 && args[1] == "-e")
+        
+        if (args.length > 1 && args[1].equals("-e"))
             System.out.println("Edges: " + T.toString());
             
     }
